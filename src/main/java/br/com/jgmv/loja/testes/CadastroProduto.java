0c5f10ace1a0968daf0f0ab0e66e3c760a5e62d0
GabrielMVieira <gabrielmvieira1296@gmail.com>
2023-09-27 14:31:31 -0300

    Refatoração do código de CadastroProduto:
    - Substitui a criação direta de um objeto Produto por um construtor aprimorado.
    - Utiliza a classe ProdutoDao para persistir o Produto no banco de dados.
    - Melhora a organização do código e o uso de JPAUtil para obter o EntityManager.
    - Atualiza para o modelo do Iphone 12 e a Categoria correspondente.

--- src/main/java/br/com/jgmv/loja/testes/CadastroProduto.java
+++ src/main/java/br/com/jgmv/loja/testes/CadastroProduto.java
@@ -1,29 +1,28 @@
 package br.com.jgmv.loja.testes;
 
+import br.com.jgmv.loja.dao.ProdutoDao;
+import br.com.jgmv.loja.modelo.Categoria;
 import br.com.jgmv.loja.modelo.Produto;
+import br.com.jgmv.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+
 import java.math.BigDecimal;
 
 public class CadastroProduto {
 
     public static void main(String[] args) {
 
-        Produto celular = new Produto();
+        Produto celular = new Produto("Iphone 12", "Proteção IP68", new BigDecimal(4000), Categoria.CELULARES);
 
-        celular.setNome("Iphone 11");
-        celular.setDescricao("Aparelho com processador A13");
-        celular.setPreco(new BigDecimal(3000));
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("mercado");
-        EntityManager em = factory.createEntityManager();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao pd = new ProdutoDao(em);
         EntityTransaction transaction = em.getTransaction();
 
         transaction.begin();
-        em.persist(celular);
+        pd.cadastrar(celular);
         transaction.commit();
         em.close();
 
