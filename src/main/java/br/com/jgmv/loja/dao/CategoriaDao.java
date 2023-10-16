35d7c01cd04a48cb55ce69b396ce3ec55aaeb361
GabrielMVieira <gabrielmvieira1296@gmail.com>
2023-10-16 16:26:40 -0300

    refactor: identacao

--- src/main/java/br/com/jgmv/loja/dao/CategoriaDao.java
+++ src/main/java/br/com/jgmv/loja/dao/CategoriaDao.java
@@ -12,18 +12,16 @@ public class CategoriaDao {
     public CategoriaDao(EntityManager em) {
         this.em = em;
     }
-    public void cadastrar (Categoria categoria){
-    public void cadastrar(Categoria categoria) {
 
+    public void cadastrar(Categoria categoria) {
         //Associa a entidade ao contexto de persistência (move para o estado managed)
         this.em.persist(categoria);
     }
 
-    public void atualizar (Categoria categoria){
+    public void atualizar(Categoria categoria) {
         this.em.merge(categoria);
     }
 
-    public void remover (Categoria categoria){
     public void desanexar(Categoria categoria) {
         if (this.em.contains(categoria)) {
             this.em.detach(categoria);
